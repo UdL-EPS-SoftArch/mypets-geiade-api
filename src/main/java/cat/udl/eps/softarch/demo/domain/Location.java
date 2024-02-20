c6f9fbc1d0aa1d6f6508a12e0c920972c66f2fb7
Angel Baro <angelbaro2001@gmail.com>
2024-02-20 15:57:27 +0100

    Created the different attributes of the class

---
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Location.java
@@ -0,0 +1,41 @@
+package cat.udl.eps.softarch.demo.domain;
+
+import jakarta.persistence.Entity;
+import com.fasterxml.jackson.annotation.JsonIdentityReference;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+@Entity
+public class Location {
+
+    @Id
+    @GeneratedValue()
+    private Integer id;
+
+    @NotNull
+    @NotBlank
+    private String address;
+
+    @NotNull
+    @NotBlank
+    private Float latitude;
+
+    @NotNull
+    @NotBlank
+    private Float longitude;
+
+    @NotNull
+    @NotBlank
+    private String province;
+
+    @NotNull
+    @NotBlank
+    private String city;
+
+    @NotNull
+    @NotBlank
+    private Integer postalCode;
+}
