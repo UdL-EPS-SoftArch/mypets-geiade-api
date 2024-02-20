b32928e2cfa1faf0f9dd19c90f145dd38faa12bf
Angel Baro <angelbaro2001@gmail.com>
2024-02-20 16:04:00 +0100

    Added EqualsAndHashCode

--- src/main/java/cat/udl/eps/softarch/demo/domain/Location.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Location.java
@@ -6,9 +6,11 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
 @Entity
+@EqualsAndHashCode(callSuper = false)
 public class Location {
 
     @Id
