b7fe16e198d346f1d1880f13c808d0f474c710bd
LordDangerous <mamrayo10@gmail.com>
2024-02-20 16:11:01 +0100

    Added new class Shelter

---
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Shelter.java
@@ -0,0 +1,44 @@
+package cat.udl.eps.softarch.demo.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.Data;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jdk.jfr.BooleanFlag;
+import lombok.EqualsAndHashCode;
+import org.hibernate.validator.constraints.Length;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class Shelter extends UriEntity<String> {
+
+    @Id
+    private String id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank
+    @Length(min=12, max=12)
+    private String mobile;
+
+    @NotBlank
+    private LocalDateTime createdAt;
+
+    @NotBlank
+    private LocalDateTime updatedAt;
+
+    @BooleanFlag
+    private boolean isActive;
+
+}
