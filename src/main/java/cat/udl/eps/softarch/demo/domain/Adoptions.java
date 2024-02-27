a535dafa81f4b1210eb05128554772b6fc9469ca
gerardmallol <gerardmallol01@gmail.com>
2024-02-27 15:29:55 +0100

    Adoption class

--- src/main/java/cat/udl/eps/softarch/demo/domain/Adoptions.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Adoptions.java
@@ -2,6 +2,7 @@ package cat.udl.eps.softarch.demo.domain;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
@@ -18,15 +19,28 @@ import java.time.LocalDateTime;
 @EqualsAndHashCode (callSuper = false)
 
 public class Adoptions {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @OneToOne
+    @NotNull
+    @NotBlank
+    private User user;
+
+    @OneToOne
+    @NotNull
+    @NotBlank
+    private Pet pet;
+
     @NotNull
     @NotBlank
     private LocalDateTime dateOfAdoption;
-    @Id
-    @GeneratedValue
-    private int id;
 
-    public Adoptions() {
+    public Adoptions (User user, Pet pet) {
+        this.user = user;
+        this.pet = pet;
         this.dateOfAdoption = LocalDateTime.now();
     }
-
 }
