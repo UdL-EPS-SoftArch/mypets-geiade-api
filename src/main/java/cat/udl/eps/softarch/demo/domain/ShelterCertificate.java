646a0519c9064799c377979c8f5f4aa612317729
Angel Baro <angelbaro2001@gmail.com>
2024-02-29 15:44:02 +0100

    OneToOne relationship with Shelter

--- src/main/java/cat/udl/eps/softarch/demo/domain/ShelterCertificate.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/ShelterCertificate.java
@@ -1,7 +1,9 @@
 package cat.udl.eps.softarch.demo.domain;
 
+import com.fasterxml.jackson.annotation.JsonIdentityReference;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -12,6 +14,10 @@ import java.time.LocalDateTime;
 @Entity
 @EqualsAndHashCode(callSuper = false)
 public class ShelterCertificate {
+    @OneToOne
+    @NotNull
+    @JsonIdentityReference(alwaysAsId = true)
+    private Shelter shelter;
 
     @Id
     private Long id;
