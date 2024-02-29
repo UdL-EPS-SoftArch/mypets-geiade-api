3fbfb0449487ff341af959c66a09c1342003e7d6
gerardmallol <gerardmallol01@gmail.com>
2024-02-29 15:56:53 +0100

    permission relationship

--- src/main/java/cat/udl/eps/softarch/demo/domain/Permission.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Permission.java
@@ -1,19 +1,26 @@
 package cat.udl.eps.softarch.demo.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Setter
+@Getter
 @Data
 @EqualsAndHashCode(callSuper = false)
 public class Permission {
 
     @Id
+    @GeneratedValue
+    @NotNull
     private Long id;
 
     @NotBlank
