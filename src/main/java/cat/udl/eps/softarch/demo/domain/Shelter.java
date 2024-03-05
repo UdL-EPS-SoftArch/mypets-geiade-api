cf62fb8064b6348f2b008c7f212514da8a912813
LordDangerous <mamrayo10@gmail.com>
2024-03-05 15:12:31 +0100

    [CHORE] Refactor unused imports

--- src/main/java/cat/udl/eps/softarch/demo/domain/Shelter.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/Shelter.java
@@ -2,14 +2,13 @@ package cat.udl.eps.softarch.demo.domain;
 
 import com.fasterxml.jackson.annotation.JsonIdentityReference;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
-import lombok.Data;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jdk.jfr.BooleanFlag;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
-import org.hibernate.validator.constraints.Length;
 
 import java.time.LocalDateTime;
 
