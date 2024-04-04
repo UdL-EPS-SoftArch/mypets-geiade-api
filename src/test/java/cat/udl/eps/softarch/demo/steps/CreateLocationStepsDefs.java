5b1486c002ba1f408ee608a54be254adb2bfe20c
Angel Baro <angelbaro2001@gmail.com>
2024-04-04 16:02:01 +0200

    Added the scenarios/features and the implmeentation of them

--- src/test/java/cat/udl/eps/softarch/demo/steps/CreateLocationStepsDefs.java
+++ src/test/java/cat/udl/eps/softarch/demo/steps/CreateLocationStepsDefs.java
@@ -6,13 +6,24 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
 
 public class CreateLocationStepsDefs {
 
@@ -24,32 +35,68 @@ public class CreateLocationStepsDefs {
 
     public static String newResourceUri;
 
-    @When("I create a new Location with the following details:$")
-    public void iCreateANewSeedWithScientificNameAndCommonName(Map<String, String> locationDetails) throws Throwable {
+    @When("There is already a Location with the following details:$")
+    public void thereIsAlreadyALocationWithTheFollowingDetails(Map<String, String> locationDetails) throws Throwable{
         Location location = new Location();
         location.setAddress(locationDetails.get("address"));
-        location.setLatitude(Float.parseFloat(locationDetails.get("latitude")));
-        location.setLongitude(Float.parseFloat(locationDetails.get("longitude")));
         location.setProvince(locationDetails.get("province"));
         location.setCity(locationDetails.get("city"));
         location.setPostalCode(Integer.parseInt(locationDetails.get("postalCode")));
-
-        stepDefs.result = stepDefs.mockMvc.perform(
-                        post("/locations")
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content(stepDefs.mapper.writeValueAsString(location))
-                                .characterEncoding(StandardCharsets.UTF_8)
-                                .accept(MediaType.APPLICATION_JSON)
-                                .with(AuthenticationStepDefs.authenticate()))
-                .andDo(print());
-        newResourceUri = stepDefs.result.andReturn().getResponse().getHeader("Location");
+        locationRepository.save(location);
     }
+    @When("I create a new Location with the following details:$")
+    public void iCreateANewLocationWithTheFollowingDetails(Map<String, String> locationDetails) throws Throwable {
+        Location existingLocation = locationRepository.findLocationByAddressAndProvinceAndCityAndPostalCode(
+                locationDetails.get("address"),
+                locationDetails.get("province"),
+                locationDetails.get("city"),
+                Integer.parseInt(locationDetails.get("postalCode"))
+        );
+
+        if (existingLocation == null) {
+
+            Location location = new Location();
+            location.setAddress(locationDetails.get("address"));
+            location.setProvince(locationDetails.get("province"));
+            location.setCity(locationDetails.get("city"));
+            location.setPostalCode(Integer.parseInt(locationDetails.get("postalCode")));
+
+            // Verify and assign latitude and longitude if exists
+            if (locationDetails.containsKey("longitude") && locationDetails.get("longitude") != null) {
+                location.setLongitude(Float.parseFloat(locationDetails.get("longitude")));
+            }
+
+
+            if (locationDetails.containsKey("latitude") && locationDetails.get("latitude") != null) {
+                location.setLatitude(Float.parseFloat(locationDetails.get("latitude")));
+            }
+
+            stepDefs.result = stepDefs.mockMvc.perform(
+                            post("/locations")
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .content(stepDefs.mapper.writeValueAsString(location))
+                                    .characterEncoding(StandardCharsets.UTF_8)
+                                    .accept(MediaType.APPLICATION_JSON)
+                                    .with(AuthenticationStepDefs.authenticate()))
+                    .andDo(print());
+            newResourceUri = stepDefs.result.andReturn().getResponse().getHeader("Location");
+
+        } else {
+            assertThat(existingLocation).isNotNull();
+        }
+    }
+
+
 
     @And("There is (\\d+) Location created$")
     public void thereIsLocationCreated(int locationCreatedNum) {
         Assert.assertEquals(locationCreatedNum, locationRepository.count());
     }
 
+    @And("There is only (\\d+) Location with the details:$")
+    public void thereIsOnlyLocationWithTheDeatils(int locationCreatedNum, Map<String, String> locationDetails) {
+        Assert.assertEquals(locationCreatedNum, locationRepository.count());
+    }
 
 
 }
--- src/test/resources/features/CreateLocation.feature
+++ src/test/resources/features/CreateLocation.feature
@@ -20,31 +20,44 @@ Feature: Create Location
   Scenario: Create a Location with an existing address, city, province, and postal code
     Given I can login with username "username" and password "password"
     And There is already a Location with the following details:
-      | address       | Major Street 3 |
-      | city          | Seròs          |
-      | province      | Lleida         |
-      | postalCode    | 25183          |
-    When I attempt to create a new Location with the following details:
+      | address       | Major Street 3     |
+      | province      | Lleida             |
+      | city          | Seròs              |
+      | postalCode    | 25183              |
+    When I create a new Location with the following details:
       | address       | Major Street 3     |
       | latitude      | 41.6167            |
       | longitude     | 0.6222             |
       | province      | Lleida             |
       | city          | Seròs              |
       | postalCode    | 25183              |
-    Then The response code is 409
-    And There is 0 Location created
+    Then The response code is 200
+    And There is only 1 Location with the details:
+      | address       | Major Street 3     |
+      | province      | Lleida             |
+      | city          | Seròs              |
+      | postalCode    | 25183              |
 
   Scenario: Attempt to create a Location with address blank and latitude null
     Given I can login with username "username" and password "password"
     When I create a new Location with the following details:
       | address       |                  |
-      | latitude      | null             |
       | longitude     | -74.0060         |
       | province      | Lleida           |
       | city          | Seròs            |
       | postalCode    | 25183            |
     Then The response code is 400
     And There is 0 Location created
+
+  Scenario: Attempt to create a Location with longitude null and latitude null
+    Given I can login with username "username" and password "password"
+    When I create a new Location with the following details:
+      | address       | Major Street 3   |
+      | province      | Lleida           |
+      | city          | Seròs            |
+      | postalCode    | 25183            |
+    Then The response code is 201
+    And There is 1 Location created
 
   Scenario: Attempt to create a Location without being logged in
     Given I'm not logged in
