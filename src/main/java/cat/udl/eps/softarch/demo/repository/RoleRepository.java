0fd6126a06e5b76b94a34221fe3d3a67ff7cb46e
Hdelegido <hectordelegido2001@gmail.com>
2024-04-05 22:15:32 +0200

    RolesStepDefs

--- src/main/java/cat/udl/eps/softarch/demo/repository/RoleRepository.java
+++ src/main/java/cat/udl/eps/softarch/demo/repository/RoleRepository.java
@@ -8,4 +8,5 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 public interface RoleRepository extends CrudRepository<Role, Long>, PagingAndSortingRepository<Role, Long> {
+    Role findByName(String oldName);
 }
---
+++ src/test/java/cat/udl/eps/softarch/demo/steps/ManageRolesStepDefs.java
@@ -0,0 +1,91 @@
+package cat.udl.eps.softarch.demo.steps;
+
+import cat.udl.eps.softarch.demo.domain.Role;
+import cat.udl.eps.softarch.demo.repository.RoleRepository;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+
+import java.nio.charset.StandardCharsets;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.mockito.Mockito.when;
+
+public class ManageRolesStepDefs {
+
+    @Autowired
+    private StepDefs stepDefs;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    private Role createdRole;
+    private Role existingRole;
+
+    @Given("There is a role with name {string}")
+    public void thereIsARoleWithName(String roleName) {
+        existingRole = new Role();
+        existingRole.setName(roleName);
+
+        // Mocking the behavior of the repository to return the existing role
+        when(roleRepository.findByName(roleName)).thenReturn(existingRole);
+    }
+
+    @When("^I create a role with name \"([^\"]*)\"$")
+    public void iCreateARoleWithName(String roleName) throws Exception {
+        Role role = new Role();
+        role.setName(roleName);
+
+        createdRole = roleRepository.save(role);
+
+        stepDefs.result = stepDefs.mockMvc.perform(
+                        post("/roles")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(stepDefs.mapper.writeValueAsString(role))
+                                .characterEncoding(StandardCharsets.UTF_8)
+                                .accept(MediaType.APPLICATION_JSON)
+                                .with(AuthenticationStepDefs.authenticate()))
+                .andDo(print());
+    }
+
+    @When("^I update the role with name \"([^\"]*)\" to have name \"([^\"]*)\"$")
+    public void iUpdateTheRoleWithNameToHaveName(String oldName, String newName) throws Exception {
+        Role role = roleRepository.findByName(oldName);
+        role.setName(newName);
+
+        stepDefs.result = stepDefs.mockMvc.perform(
+                        put("/roles/{id}", role.getId())
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(stepDefs.mapper.writeValueAsString(role))
+                                .characterEncoding(StandardCharsets.UTF_8)
+                                .accept(MediaType.APPLICATION_JSON)
+                                .with(AuthenticationStepDefs.authenticate()))
+                .andDo(print());
+    }
+
+    @When("^I delete the role with name \"([^\"]*)\"$")
+    public void iDeleteTheRoleWithName(String roleName) throws Exception {
+        Role role = roleRepository.findByName(roleName);
+
+        stepDefs.mockMvc.perform(
+                        delete("/roles/{id}", role.getId())
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaType.APPLICATION_JSON)
+                                .with(AuthenticationStepDefs.authenticate()))
+                .andDo(print());
+    }
+
+    @When("I retrieve the role with name {string}")
+    public void iRetrieveTheRoleWithName(String roleName) throws Exception {
+        // Implementation already exists in the RoleStepDefs class
+    }
+
+    @Then("^The response code is (\\d+)$")
+    public void theResponseCodeIs(int statusCode) throws Exception {
+        stepDefs.result.andExpect(status().is(statusCode));
+    }
+}
