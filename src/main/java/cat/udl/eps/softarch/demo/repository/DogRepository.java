c2e08e766f3f811814a5daa6bef76af969dc3f16
PauMAVA <paumachetti@gmail.com>
2024-05-18 18:49:05 +0200

    Added missing functions to search cats and dogs by name and chip

--- src/main/java/cat/udl/eps/softarch/demo/repository/DogRepository.java
+++ src/main/java/cat/udl/eps/softarch/demo/repository/DogRepository.java
@@ -3,8 +3,15 @@ package cat.udl.eps.softarch.demo.repository;
 import cat.udl.eps.softarch.demo.domain.Dog;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
 
 @RepositoryRestResource
 public interface DogRepository extends CrudRepository<Dog, Long>, PagingAndSortingRepository<Dog, Long> {
+
+    List<Dog> findByName(@Param("name") String name);
+    List<Dog> findByChip(@Param("chip") String name);
+
 }
